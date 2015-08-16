0ce5e37772662d49c31762d4ed92df1d34206684
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2015-08-16 23:38:31 +1200

    ability to diff b/w mac and win

--- src/com/gollahalli/main/controller/controller.java
+++ src/com/gollahalli/main/controller/controller.java
@@ -1,7 +1,26 @@
 package com.gollahalli.main.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.BorderPane;
+
 /**
  * Created by akshayrajgollahalli on 16/08/15.
  */
 public class controller {
+
+    @FXML
+    private MenuBar jmark_menuBar;
+    @FXML
+    private BorderPane jmark_borderPane;
+
+    @FXML
+    private void initialize(){
+        String os = System.getProperty("os.name");
+        if (os != null && os.startsWith("Mac")){
+            jmark_menuBar.useSystemMenuBarProperty().set(true);
+            jmark_borderPane.setPrefSize(800,600);
+            jmark_borderPane.setLayoutY(0.0);
+        }
+    }
 }
