c773e21888900063756e0942163017fc42e3b9f4
Akshay Raj Gollahalli <akshaybabloo@gmail.com>
2015-08-16 22:20:23 +1200

    updated to open GUI

--- src/com/gollahalli/main/Main.java
+++ src/com/gollahalli/main/Main.java
@@ -1,8 +1,27 @@
 package com.gollahalli.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/gollahalli/gui/JMark_gui.fxml"));
+        Scene scene = new Scene(root, 800, 600);
+
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("JMark");
+        primaryStage.show();
+
+
     }
 }
